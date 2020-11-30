ceb1509da827c8cf7378f84e62f58b9317daa4d7
William Hermann <whermann4@gmail.com>
2020-11-30 15:47:12 -0800

    updated madlib and initally done for grading.

--- labs/grading.py
+++ labs/grading.py
@@ -1,4 +1,26 @@
-use_grade = int(input('please enter a number grade: '))
-
 def grades(number):
+    if number >= 90:
+        print('you got an A')
+    elif number >= 80:
+        print('you got an B')
+    elif number >= 70:
+        print('you got an C')
+    elif number >= 60:
+        print('you got an D')
+    elif number <= 59:
+        print('you got an F')
+
+while True:
+    grad_quest = input('Would you like to convert your grade? \n yes or no: ')
+    if grad_quest == 'no':
+        print('Have a good day')
+        break
+    use_grade = int(input('please enter a number grade: '))
+    grades(use_grade)
+
+
+
+
+
+
     
--- labs/madlib.py
+++ labs/madlib.py
@@ -1,9 +1,11 @@
 
 
-game_time = input('do you want to make a madlib?: \n yes or no: ')
-while game_time == 'yes':
+
+while True:
+    game_time = input('do you want to make a madlib?: \n yes or no: ')
     if game_time == 'no':
         print('have a good day')
+        break
     number = int(input('Pick a number: '))
     occupation = input('pick a occupation: ')
     occupation1 = input('Pick another occupation: ')
@@ -18,6 +20,6 @@ while game_time == 'yes':
     verb = input('pick a verb: ')
     verb1 = input('pick another verb: ')
     print(f'{number} years after the end of Rush Hour 2, James Carter is no longer a {occupation}, but a {occupation1} on the streets of {place1}. Lee is now the bodyguard for his friend {person1}. Lee is still upset with Carter about an incident in {place1} when Carter accidentally shot Lee`s girlfriend, {occupation1} Isabella Molina, in the {body_part}During the World Criminal Court discussions, as {person1} addresses the importance to fight the Triad, he announces that he knows the {adjective} of the Triad leadership known as the Shy Shen. Suddenly, {person1} takes a {noun} in the {body_part1}, disrupting the conference. Lee pursues the assassin and corners him, discovering that the assassin is his brother, {celebrity}. When Lee hesitates to shoot {celebrity}, Carter shows up {verb} towards the two and {adverb} {verb1} Lee over, allowing {celebrity} to escape.')
-    game_time = input('do you want to play again? \n yes or no: ')
-
+    
+    
 

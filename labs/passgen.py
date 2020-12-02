3088505150c7fc1920b03dfdaf4c7c6a4bb7af8c
William Hermann <whermann4@gmail.com>
2020-12-02 11:03:43 -0800

    updated

--- labs/passgen.py
+++ labs/passgen.py
@@ -1,3 +1,4 @@
+# import
 import random
 import string
 letters = string.ascii_letters
@@ -5,18 +6,17 @@ numbers = string.digits
 puncs = string.punctuation
 mast = letters + numbers + puncs
 usable = list(mast)
+#user input for the number of characters desired in the password
 
-
-useput = int(input('please enter a number for your password: '))
-
-
-def passgen():
+#random password function
+def passgen(n):
     pass_set = []
     x = 0
-    while x < useput:
+    while x < n:
         x += 1
         pass_set.append(random.choice(usable))
     pass_set = "".join(pass_set)
     return pass_set
 
-print(passgen())
+useput = int(input('please enter a number of characters for your password: '))
+print(passgen(useput))

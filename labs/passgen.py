cc0205ce441b0e3d9648acd2b5dad318ca0fed5c
William Hermann <whermann4@gmail.com>
2020-12-02 09:37:58 -0800

    password lab completed

--- labs/passgen.py
+++ labs/passgen.py
@@ -1,12 +1,11 @@
 import random
 import string
 letters = string.ascii_letters
-#upper_letters = string.ascii_uppercase
 numbers = string.digits
 puncs = string.punctuation
 mast = letters + numbers + puncs
 usable = list(mast)
-#print(usable)
+
 
 useput = int(input('please enter a number for your password: '))
 
@@ -17,7 +16,6 @@ def passgen():
     while x < useput:
         x += 1
         pass_set.append(random.choice(usable))
-        #chars = random.choice(usable)
     pass_set = "".join(pass_set)
     return pass_set
 

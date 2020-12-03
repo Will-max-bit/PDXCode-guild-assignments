ce55cd16ea89aae62d0510d63c3c27293ace71c7
William Hermann <whermann4@gmail.com>
2020-12-02 22:01:48 -0800

    finished optional

--- labs/guessnumber.py
+++ labs/guessnumber.py
@@ -48,14 +48,50 @@ while True:
         break
 '''
 #abs(current_guess-target)
-
+x = 0
+gues_keeper = []
 while True:
     guess = int(input('What is your guess? '))
+    if x >= 1:
+        x += 1
+    gues_keeper.append(guess)
     if guess != hidden_number:
         if guess < hidden_number:
-            print('That is to low, try again' )
-        elif guess > hidden_number:
-            print('That is to high, try again')
+            if x < 1:
+                x += 1
+                print('To low')
+                continue
+            if x >= 1:
+                if gues_keeper[1-x] > gues_keeper[2-x]:
+                    print('You are getting closer')
+                elif gues_keeper[1-x] < gues_keeper[2-x]:
+                    print('You are getting further away') #line below starts the other side
+        
+
+        if guess > hidden_number:
+            if x < 1:
+                x += 1
+                print('to high')
+                continue
+            if x >= 1:
+                if gues_keeper[1-x] < gues_keeper[2-x]:
+                    print('You are getting closer')
+                elif gues_keeper[1-x] > gues_keeper[2-x]:
+                    print('You are getting further away')        #elif guess > hidden_number:
+        #x += 1            #print('That is to high, try again')
     if guess == hidden_number:
         print('You guessed the right number, good job.')
+        print(abs(hidden_number))
         break
+'''
+gues_keeper = []
+while True:
+    guess = int(input('What is your guess? '))
+    gues_keeper.append(guess)
+    if guess != hidden_number:
+        print(f'That is incorrect, try again' )
+    if guess == hidden_number:
+        print('You guessed the right number, good job.')
+        print(f'You guessed {len(gues_keeper)} times, gotta pump those numbers down')
+        break
+'''

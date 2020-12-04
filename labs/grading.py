57edb7ab62a9f281e215f8fedbfd985107194e7a
William Hermann <whermann4@gmail.com>
2020-12-03 18:06:56 -0800

    updated grading with comments

--- labs/grading.py
+++ labs/grading.py
@@ -1,7 +1,8 @@
 
 import string
-# function to convert number grades into letter grades and print result
+    # function to convert number grades into letter grades and print result
 def grades(number):
+    # function will take one parameter and then compare that through if statements, when match is found the program will print the result for the user.
     if number >= 90:
         if number % 10 > 5: 
             print('you got an A+')
@@ -32,12 +33,14 @@ def grades(number):
             print('You got an D')
     elif number <= 59:
         print('you got an F')
-#While loop for user input
+    
 while True:
+    #while loop parameter is attached to grad_quest, when grad_quest input is 'no' the program will print a message then end
     grad_quest = input('Would you like to convert your grade? \n yes or no: ')
     if grad_quest == 'no':
         print('Have a good day')
         break
+    #usegrade variable input will be used as the parameter for the grades function
     use_grade = int(input('please enter a number grade: '))
     grades(use_grade)
 

2b37b7099e7bea3dee3f84c265fca37fba9ea734
Will-max-bit <whermann4@gmail.com>
2020-12-23 18:47:55 -0800

    commented and edited lab19.

--- labs/lab19_Rain_data.py
+++ labs/lab19_Rain_data.py
@@ -5,7 +5,7 @@ import pprint
 import math
 import statistics
 
-
+#Function opens data file and parses the data for date and first value
 def get_data():
     with open('Shipyard_Rain_Gage.rain', 'r') as file:
         text = file.read()
@@ -17,7 +17,7 @@ def get_data():
         data.append((date, daily_total))
     return data
 
-
+#function creates a 0 variable that will be used to total the rain amount data
 def calculate_mean(data):
     mean_tally = 0
     for dat in data:
@@ -26,12 +26,11 @@ def calculate_mean(data):
     return mean_total 
 
 
-
+#item[1] is the rain amount value index of data, value of item[1] will be used to sort
 def getkey(item):
     return item[1]
 
- #words.sort(key=lambda tup: tup[1], reverse=True)
-
+#using variance from the statistics module to get the variance
 data = get_data()
 def variance():
     nums = []
@@ -39,15 +38,23 @@ def variance():
         nums.append(datum[1])
     return ("The variance of water is % s"%(statistics.variance(nums)))
 
-
+#sorted the list of tuples (data) and reversing to make the highest number first
 date_and_water = sorted(data, key=getkey, reverse=True)
+
+def water_most(x):
+    water_data = x[0]
+    dtg = water_data[0]
+    dtg = date_and_water[0]
+    date_group = (dtg[0].strftime('%d-%b-%Y'))
+    return (f'on {date_group} there was {water_data[1]} inches of water')
+
+
+print(f'The mean is {calculate_mean(data)}inches')
 print(variance())
-water_data = date_and_water[0]
-dtg = date_and_water[0]
-date_group = (dtg[0].strftime('%d-%b-%Y'))
-print(f'on {date_group} there was {water_data[1]} inches of water')
-mean = calculate_mean(data)
-print(f'The mean is {mean}')
+print(water_most(date_and_water))
+
+
+
 
 
 

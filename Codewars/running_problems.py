dc3e7b41d133eb399636a22fb16c609f25a572d5
Will-max-bit <whermann4@gmail.com>
2020-12-27 14:49:54 -0800

    new kata completed.

--- Codewars/running_problems.py
+++ Codewars/running_problems.py
@@ -127,3 +127,72 @@ test = string.ascii_lowercase
     
 
 # print(even_or_odd(test))
+# Trolls are attacking your comment section!
+# A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+# Your task is to write a function that takes a string and return a new string with all vowels removed.
+# For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+# Note: for this kata y isn't considered a vowel.
+
+# import re
+## refactored after research
+
+# def disemvowel(string):
+#     x = re.sub(r'[aeiou]','',string, flags=re.IGNORECASE)
+#     return x
+
+
+# test = 'This website is for losers LOL!'
+
+# print(disemvowel(test))
+# exit()
+
+
+# def disemvoweled(string):
+#     vowels = ['a','e','i','o','u','A','E','I','O','U']
+#     output = []
+#     sentence = string
+#     for sen in sentence:
+#         if sen not in vowels:
+#             output.append(sen)
+#         else:
+#             continue
+#     return (''.join(output))
+
+
+# print(disemvoweled(test))
+
+# Given a string of words, you need to find the highest scoring word.
+# Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+# You need to return the highest scoring word as a string.
+# If two words score the same, return the word that appears earliest in the original string.
+# All letters will be lowercase and all inputs will be valid.
+
+def word_scorer(string):
+    alpha_dict = {
+        'a':1,
+        'b':2,
+        'c':3,
+        'd':4,
+        'e':5,
+        'f':6,
+        'g':7,
+        'h':8,
+        'i':9,
+        'j':10,
+        'k':11,
+        'l':12,
+        'm':13,
+        'n':14,
+        'o':15,
+        'p':16,
+        'q':17,
+        'r':18,
+        's':19,
+        't':20,
+        'u':21,
+        'v':22,
+        'w':23,
+        'x':24,
+        'y':25,
+        'z':26,
+    }

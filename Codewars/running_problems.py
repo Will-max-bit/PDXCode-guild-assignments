d25667d046a5b0c20b1e368c42c9414627dd1576
Will-max-bit <whermann4@gmail.com>
2020-12-28 17:42:08 -0800

    removed type from running_problems

--- Codewars/running_problems.py
+++ Codewars/running_problems.py
@@ -167,32 +167,5 @@ test = string.ascii_lowercase
 # If two words score the same, return the word that appears earliest in the original string.
 # All letters will be lowercase and all inputs will be valid.
 
-def word_scorer(string):
-    alpha_dict = {
-        'a':1,
-        'b':2,
-        'c':3,
-        'd':4,
-        'e':5,
-        'f':6,
-        'g':7,
-        'h':8,
-        'i':9,
-        'j':10,
-        'k':11,
-        'l':12,
-        'm':13,
-        'n':14,
-        'o':15,
-        'p':16,
-        'q':17,
-        'r':18,
-        's':19,
-        't':20,
-        'u':21,
-        'v':22,
-        'w':23,
-        'x':24,
-        'y':25,
-        'z':26,
-    }
+# x = 0
+# print(x)

8ff39c5c83c301f7fe1728fece15086d9f030fd1
Will-max-bit <whermann4@gmail.com>
2021-01-12 18:10:09 -0800

    V2 committed

--- labs/js/unitConverter.js
+++ labs/js/unitConverter.js
@@ -7,12 +7,25 @@ dictConversions = {
     'mt': 1,
 }
 
-let unit1 = prompt('First unit of measurement i.e ft-mi-km-yd-in-mt')
-let unit2 = prompt('Second unit of measurement i.e ft-mi-km-yd-in-mt')
-let numberChoice = prompt('pick a number')
+// let unit1 = prompt('First unit of measurement i.e ft-mi-km-yd-in-mt')
+// let unit2 = prompt('Second unit of measurement i.e ft-mi-km-yd-in-mt')
+// let numberChoice = prompt('pick a number')
 
-let solution = (dictConversions[unit1] * numberChoice) / dictConversions[unit2]
-alert(solution)
+// let solution = (dictConversions[unit1] * numberChoice) / dictConversions[unit2]
+// alert(solution)
 
+let first_unit = document.querySelector('#first_unit')
+let second_unit = document.querySelector('#second_unit')
+let number_length = document.querySelector('#number_length')
+let solution = document.querySelector('#solution')
 
+solution.style.color = 'lightgrey'
 
+document.getElementById('btn_click').addEventListener('click', function () {
+    console.log(first_unit.value)
+    console.log(second_unit.value)
+    console.log(number_length.value)
+    let output = (dictConversions[first_unit.value] * number_length.value) / dictConversions[second_unit.value]
+    console.log(output)
+    solution.innerText = output
+})

2fbf0914322c238a6578dc4137b9657b768e8537
Will-max-bit <whermann4@gmail.com>
2021-01-13 17:39:15 -0800

    Rock paper scissors lab converted to js/html

--- labs/js/rockPaperScissors.js
+++ labs/js/rockPaperScissors.js
@@ -1,37 +1,61 @@
+let rock_option = document.querySelector('#rock_option')
+let paper_option = document.querySelector('#paper_option')
+let scissors_option = document.querySelector('#scissors_option')
+let btn_click = document.querySelector('#btn_click')
+let solution = document.querySelector('#solution')
 
 
 let options = ['rock', 'paper', 'scissors']
+function rockPaperScissors() {
+    while (true) {
+        let rand_Choice = Math.floor(Math.random() * options.length)
+        let compChoice = options[rand_Choice]
+        let userChoice;
+            if (paper_option.checked){
+                userChoice = paper_option.value
+            }
+            else if (rock_option.checked){
+                userChoice = rock_option.value
+            }
+            else if (scissors_option.checked) {
+                userChoice = scissors_option.value
+            }
 
-while (true) {
-    let rand_Choice = Math.floor(Math.random() * options.length)
-    let compChoice = options[rand_Choice]
-    let userChoice = prompt('Pick between rock paper or scissors').toLowerCase();
+        if (userChoice === compChoice) {
+            return ("It's a tie")
+        }
+        else if (userChoice === 'rock' && compChoice === 'paper') {
+            return("You have lost")
+        }
 
-    if (userChoice === compChoice) {
-        alert("It's a tie")
+        else if (userChoice === 'rock' && compChoice !== 'scissors') {
+            return ("You have won")
+        }
+
+        else if (userChoice === 'paper' && compChoice !== 'scissors') {
+            return ("You have lost")
+        }
+
+        else if (userChoice === 'paper' && compChoice !== 'rock') {
+            return ("You have won")
+        }
+
+        else if (userChoice === 'scissors' && compChoice !== 'rock') {
+            return ("You have lost")
+        }
+
+        else if (userChoice === 'scissors' && compChoice !== 'paper') {
+            return ("You have won")
+        }
+
     }
-        else if (userChoice === 'rock' && compChoice === 'paper'){
-            alert("You have lost")
-        }
-
-        else if (userChoice === 'rock' && compChoice !== 'scissors'){
-            alert("You have won")
-        }
-
-        else if (userChoice === 'paper' && compChoice !== 'scissors'){
-            alert("You have lost")
-        }
-
-        else if (userChoice === 'paper' && compChoice !== 'rock'){
-            alert("You have won")
-        }
-
-        else if (userChoice === 'scissors' && compChoice !== 'rock'){
-            alert("You have lost")
-        }
-
-        else if (userChoice === 'scissors' && compChoice !== 'paper'){
-            alert("You have won")
-        }
-        
 }
+
+
+
+document.getElementById("btn_click").addEventListener('click', function () {
+    let output = rockPaperScissors()
+    solution.innerText = output
+    
+
+})

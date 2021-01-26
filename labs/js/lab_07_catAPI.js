111a0fcc2b2581b6069c1bf45e9f301dfb88041e
Will-max-bit <whermann4@gmail.com>
2021-01-25 20:29:39 -0800

    add eventlistners (vue) to select

--- labs/js/lab_07_catAPI.js
+++ labs/js/lab_07_catAPI.js
@@ -5,6 +5,7 @@ let app = new Vue({
         cat_photo: '',
         categories: 'test',
         category: '',
+        rowID: '',
     },
     methods: {
         cat_picture: function () {
@@ -13,22 +14,27 @@ let app = new Vue({
                 url: 'https://api.thecatapi.com/v1/images/search'
             }).then((response) => {
                 this.cat_photo = response.data[0].url
-                console.log(this.cat_photo)
+                // console.log(this.cat_photo)
             })
         },
         moreCats: function () {
             this.cat_picture()
-        },
+        },     
+        // https://api.thecatapi.com/v1/images/search?category_ids=1
         cat_categories: function(){
             axios({
                 method: 'get',
                 url: 'https://api.thecatapi.com/v1/categories'
             }).then((response) => {
                 this.cat_categories = response.data
-                console.log(this.cat_photo)
+                console.log(this.cat_categories)
             })
+        },
+        category_function: function category_function(rowID,$event){
+            alert('test')
+            console.log(rowID)
+            console.log($event)
         }
-        
     },
     created: function () {
         this.cat_picture()

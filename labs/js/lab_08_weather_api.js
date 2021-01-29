e728f099dff40bf0048bf4f38726486912a158d2
Will-max-bit <whermann4@gmail.com>
2021-01-28 18:06:04 -0800

    lab_08 Weather Api completed

--- labs/js/lab_08_weather_api.js
+++ labs/js/lab_08_weather_api.js
@@ -8,6 +8,10 @@ let app = new Vue({
         dateTime: '',
         icon: '',
         iconDescription: '',
+        sunset: '',
+        sunrise: '',
+        hours: '',
+        weekly: '',
 
         
     },
@@ -23,14 +27,30 @@ let app = new Vue({
                     appid: openweathermap_api_key
                 }
             }).then(response => {
-                console.log(response.data),
+                // console.log(response.data),
+                console.log(response.data.daily)
                 this.currentTemp = parseInt((response.data.current.feels_like - 273.15)* (9/5) + 32)
                 let unix_date = (response.data.current.dt)
                 let date_Time = new Date(unix_date * 1000)
                 this.dateTime= date_Time
                 // console.log(response.data.daily)
-                console.log(response.data.current.weather[0].id)
+                // console.log(response.data.current.weather[0].id)
                 this.iconDescription = response.data.current.weather[0].description
+                this.icon = "http://openweathermap.org/img/wn/" + response.data.current.weather[0].icon +'@4x.png'
+                
+                let sunriseUnix = response.data.current.sunrise
+                non_unixDate_rise = new Date(sunriseUnix * 1000)
+                let sunrise_hours = (non_unixDate_rise.getHours()- 12)
+                let sunrise_min = (non_unixDate_rise.getMinutes())
+                this.sunrise = (sunrise_hours) +':' + sunrise_min +'a.m'
+
+                let sunsetUnix = response.data.current.sunset
+                let non_unixDate = new Date(sunsetUnix  * 1000)
+                let sunset_hours = (non_unixDate.getHours()- 12)
+                let sunset_min = (non_unixDate.getMinutes())
+                this.sunset = sunset_hours +':' + sunset_min +'p.m'
+                this.weekly = response.data.daily
+                
             })
         },
         // lets make a function with a for loop for populating the icons
@@ -50,7 +70,10 @@ let app = new Vue({
             })
             
 
-        }
+        },
+        // weeklyForecast: function(){
+
+        // }
         
         
     },

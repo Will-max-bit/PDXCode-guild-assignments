2def5ab9913ad2a647dfc8583235209f0914f8df
Will-max-bit <whermann4@gmail.com>
2021-01-29 17:58:28 -0800

    Lab_08 completed, ready for refactor

--- labs/js/lab_08_weather_api.js
+++ labs/js/lab_08_weather_api.js
@@ -12,8 +12,11 @@ let app = new Vue({
         sunrise: '',
         hours: '',
         weekly: '',
+        items: 0,
+        phase: '',
 
-        
+
+
     },
     methods: {
         weather_call: function () {
@@ -28,34 +31,34 @@ let app = new Vue({
                 }
             }).then(response => {
                 // console.log(response.data),
-                console.log(response.data.daily)
-                this.currentTemp = parseInt((response.data.current.feels_like - 273.15)* (9/5) + 32)
+                // console.log(response.data.daily)
+                this.currentTemp = parseInt((response.data.current.feels_like - 273.15) * (9 / 5) + 32)
                 let unix_date = (response.data.current.dt)
                 let date_Time = new Date(unix_date * 1000)
-                this.dateTime= date_Time
+                this.dateTime = date_Time
                 // console.log(response.data.daily)
                 // console.log(response.data.current.weather[0].id)
                 this.iconDescription = response.data.current.weather[0].description
-                this.icon = "http://openweathermap.org/img/wn/" + response.data.current.weather[0].icon +'@4x.png'
-                
+                this.icon = "http://openweathermap.org/img/wn/" + response.data.current.weather[0].icon + '@4x.png'
+
                 let sunriseUnix = response.data.current.sunrise
                 non_unixDate_rise = new Date(sunriseUnix * 1000)
-                let sunrise_hours = (non_unixDate_rise.getHours()- 12)
+                let sunrise_hours = (non_unixDate_rise.getHours() - 12)
                 let sunrise_min = (non_unixDate_rise.getMinutes())
-                this.sunrise = (sunrise_hours) +':' + sunrise_min +'a.m'
+                this.sunrise = (sunrise_hours) + ':' + sunrise_min + 'a.m'
 
                 let sunsetUnix = response.data.current.sunset
-                let non_unixDate = new Date(sunsetUnix  * 1000)
-                let sunset_hours = (non_unixDate.getHours()- 12)
+                let non_unixDate = new Date(sunsetUnix * 1000)
+                let sunset_hours = (non_unixDate.getHours() - 12)
                 let sunset_min = (non_unixDate.getMinutes())
-                this.sunset = sunset_hours +':' + sunset_min +'p.m'
+                this.sunset = sunset_hours + ':' + sunset_min + 'p.m'
                 this.weekly = response.data.daily
-                
+
             })
         },
         // lets make a function with a for loop for populating the icons
         // maybe do display the weekly weather to in a bar below?
-        location_call: function(){
+        location_call: function () {
             axios({
                 method: 'get',
                 url: "https://geo.ipify.org/api/v1",
@@ -68,20 +71,40 @@ let app = new Vue({
                 this.lon = response.data.location.lng
                 this.weather_call()
             })
-            
+
 
         },
-        // weeklyForecast: function(){
+        moonPhase: function () {
+            axios({
+                method: 'get',
+                url: "https://www.icalendar37.net/lunar/api/?",
+                params: {
+                    lang: 'en',
+                    month: new Date().getMonth() + 1,
+                    year: new Date().getFullYear(),
+                    size: "100%",
+                    lightColor: "rgb(255,255,230)",
+                    shadeColor: "transparent",
+                    texturize: true,
+                }
+            }).then(response => {
+                console.log(response.data)
+                this.phase = response.data.phase[1]
+            })
+        }
 
-        // }
-        
-        
+
+
+
     },
-    created: function(){
-            // this.weather_call()
-            this.location_call()
-        },
-    
+    created: function () {
+        // this.weather_call()
+        this.location_call()
+        this.moonPhase()
+
+
+    },
+
 })
 
 

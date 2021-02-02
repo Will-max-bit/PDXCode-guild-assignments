0dfb36d6d739b497802dc475d012e2c2998e022c
Will-max-bit <whermann4@gmail.com>
2021-02-01 20:13:41 -0800

    Updated Repo with Django and updated weather lab

--- labs/js/lab_08_weather_api.js
+++ labs/js/lab_08_weather_api.js
@@ -17,6 +17,7 @@ let app = new Vue({
         histTempHigh: '',
         histTempLow:'',
         histSum:'',
+        histDate: '',
         // radarImg: '',
 
 
@@ -50,7 +51,9 @@ let app = new Vue({
                 let sunrise_hours = (non_unixDate_rise.getHours() - 12)
                 let sunrise_min = (non_unixDate_rise.getMinutes())
                 this.sunrise = (sunrise_hours) + ':' + sunrise_min + 'a.m'
-
+                // console.log(sunriseUnix)
+                let historical_date = parseInt(sunriseUnix) - 31556926
+                this.histDate = historical_date
                 let sunsetUnix = response.data.current.sunset
                 let non_unixDate = new Date(sunsetUnix * 1000)
                 let sunset_hours = (non_unixDate.getHours() - 12)
@@ -78,6 +81,10 @@ let app = new Vue({
                 this.lat = response.data.location.lat
                 this.lon = response.data.location.lng
                 this.weather_call()
+                this.historicalData()
+                console.log(this.histDate)
+
+                // call hist here
             })
             
 
@@ -104,7 +111,7 @@ let app = new Vue({
         historicalData: function(){
             axios({
                 method:'get',
-                url: "https://dark-sky.p.rapidapi.com/44.05264668240474,-123.16752578076206,1580423253",
+                url: `https://dark-sky.p.rapidapi.com/${this.lat},${this.lon},1580423253`,
                 headers: {
                     'x-rapidapi-key': darkSky,
                     'x-rapidapi-host': 'dark-sky.p.rapidapi.com'
@@ -112,12 +119,13 @@ let app = new Vue({
 
                 // params: {
                 //     longitude: this.lon,
-                //     latitude: this.lat,
+                //     latitude: this.lat, t
                 //     time: (this.unix_date)
 
                 // }
             }).then( response =>{
-                console.log(response.data)
+
+                console.log(this.historical_date)
                 this.histTempHigh = response.data.currently.temperature
                 this.histTempLow = response.data.daily.data[0].temperatureLow
                 this.histSum = response.data.currently.summary
@@ -131,6 +139,7 @@ let app = new Vue({
     },
     created: function () {
         // this.weather_call()
+        // call hist in location
         this.location_call()
         // this.radar_map()
         this.historicalData()

33b4a346ff85debd77b3905f25625b5248c15303
Will-max-bit <whermann4@gmail.com>
2021-02-04 19:43:41 -0800

    django tutorial 4 completed

---
+++ labs/django/mysite1/polls/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Question, Choice
+
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date')
+admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Choice)
--- labs/django/mysite1/polls/urls.py
+++ labs/django/mysite1/polls/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 
 from . import views
 
-
-
 app_name = 'polls'
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/, /polls/57/
+    path('<str:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/

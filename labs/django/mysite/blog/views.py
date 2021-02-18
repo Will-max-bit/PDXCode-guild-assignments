3a4242211ad0e85653c07843b5037a45c3200f75
Will-max-bit <whermann4@gmail.com>
2021-02-17 20:01:38 -0800

    Updated django girls tutorial and user login

--- labs/django/mysite/blog/views.py
+++ labs/django/mysite/blog/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect, reverse
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def post_list(request):
+    print(request.user)
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
@@ -14,7 +16,7 @@ def post_detail(request, pk):
 
 
 
-
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -29,7 +31,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
-
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -43,3 +45,6 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def home(request):
+    return HttpResponseRedirect(reverse('blog:post_list'))
--- labs/django/mysite/users/views.py
+++ labs/django/mysite/users/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
 
 def register(request):
+    # return HttpResponse('ok')
+
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
+            user = User.objects.get(username=username)
             messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
-        else:
-            form = UserRegisterForm()
+            login(request, user)
+            return redirect('blog:post_list')
+    else:
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})

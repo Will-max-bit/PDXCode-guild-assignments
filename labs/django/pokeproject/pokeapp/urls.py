7c5cd191d31133ef24df65c7ac29bdb66ceae552
Will-max-bit <whermann4@gmail.com>
2021-02-26 19:29:24 -0800

    functional search and pagination on page

--- labs/django/pokeproject/pokeapp/urls.py
+++ labs/django/pokeproject/pokeapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'pokeapp'
 urlpatterns = [
-    path('', views.load_pokemon, name='load_pokemon')
+    path('', views.load_pokemon, name='load_pokemon'),
+    path('load_vue_pokemon', views.load_vue_pokemon, name='load_vue_pokemon'),
+    path('vue_index', views.vue_index, name='vue_index' ),
 ]

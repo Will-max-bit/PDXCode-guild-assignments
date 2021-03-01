61696d3c75bacfc84e8d31bc4fc0a57743e0ceca
Will-max-bit <whermann4@gmail.com>
2021-02-28 18:14:05 -0800

    Updated styling on Pokedex app

--- labs/django/pokeproject/pokeapp/urls.py
+++ labs/django/pokeproject/pokeapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'pokeapp'
 urlpatterns = [
-    path('', views.load_pokemon, name='load_pokemon'),
+    path('load_pokemon', views.load_pokemon, name='load_pokemon'),
     path('load_vue_pokemon', views.load_vue_pokemon, name='load_vue_pokemon'),
-    path('vue_index', views.vue_index, name='vue_index' ),
+    path('', views.vue_index, name='vue_index' ),
 ]
--- labs/django/pokeproject/pokeapp/views.py
+++ labs/django/pokeproject/pokeapp/views.py
@@ -11,7 +11,7 @@ from django.db.models import Q
 
 
 
-
+# using built in Django pagination
 def load_pokemon(request):
     # return HttpResponse('ok')
 
@@ -27,19 +27,21 @@ def vue_index(request):
 
 
 def load_vue_pokemon(request):
+    # Getting the pokemon out of database
     list_pokemon = Pokemon.objects.all()
     num_pages = 1
     total = list_pokemon.count()
+    # search through database
     if 'search' in request.GET:
         search = request.GET['search']
         list_pokemon = list_pokemon.filter(Q(name__icontains=search)| Q(types__icontains=search))
-
+# determining pagination 
     if 'page' in request.GET:
         page = request.GET['page']
         paginator = Paginator(list_pokemon, 20)
         list_pokemon = paginator.page(page)
         num_pages = paginator.num_pages
-
+# grabbing poke data, assigning to a list then including data with pagination in Json response
     list_pokemon_data = []
     for pokemon in list_pokemon:
         list_pokemon_data.append({
